0179ad0db4fc74252636e039d6a02539146fc96d
Amit Goel <amit@kineti.nl>
2026-04-01 12:51:05 -0500

    meta ads

--- components/cookie-banner.tsx
+++ components/cookie-banner.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import Script from 'next/script';
 import { Analytics } from '@vercel/analytics/next';
 import { useLanguage } from '@/contexts/language-context';
 
@@ -11,14 +12,14 @@ const STORAGE_KEY = 'cookie-consent';
 const t = {
   nl: {
     message:
-      'Wij gebruiken analytische cookies om het gebruik van onze website te begrijpen en te verbeteren. Uw gegevens worden niet gedeeld met derden.',
+      'Wij gebruiken cookies voor analyse en advertentiedoeleinden (waaronder Meta/Facebook). Als u akkoord gaat, worden uw gegevens gedeeld met deze partijen.',
     accept: 'Alles accepteren',
     decline: 'Alleen noodzakelijk',
     privacy: 'Privacybeleid',
   },
   en: {
     message:
-      'We use analytical cookies to understand and improve how our website is used. Your data is not shared with third parties.',
+      'We use cookies for analytics and advertising purposes (including Meta/Facebook). If you agree, your data will be shared with these parties.',
     accept: 'Accept all',
     decline: 'Necessary only',
     privacy: 'Privacy policy',
@@ -56,8 +57,31 @@ export default function CookieBanner() {
 
   return (
     <>
-      {/* Only load analytics when explicitly accepted */}
+      {/* Only load tracking when explicitly accepted */}
       {consent === 'accepted' && <Analytics />}
+      {consent === 'accepted' && (
+        <>
+          <Script id="meta-pixel" strategy="afterInteractive">
+            {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init','1647032802996383');
+fbq('track','PageView');`}
+          </Script>
+          <noscript>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img height="1" width="1" style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=1647032802996383&ev=PageView&noscript=1"
+              alt=""
+            />
+          </noscript>
+        </>
+      )}
 
       {visible && (
         <div
